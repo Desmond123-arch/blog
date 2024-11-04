8191eeaf549d4026217f3dcba660c4a875b76f0a
desmond123-arch <desmondopokumends2004@gmail.com>
2024-11-04 01:00:45 +0000

    update env

--- pkg/database/handlers.go
+++ pkg/database/handlers.go
@@ -8,18 +8,12 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/jackc/pgx/v5/stdlib"
-	"github.com/joho/godotenv"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 )
 
 func SetupDatabase() (*bun.DB, error){
 	// urlExample := "postgres://username:password@localhost:5432/database_name"
-	err := godotenv.Load()
-	if err != nil {
-		log.Printf("Database Error: %s", err)
-		return nil, err
-	}
 	config, err := pgxpool.ParseConfig(os.Getenv("POSTGRES_URL"))
 	if err != nil {
 		log.Printf("Database Error: %s", err)
